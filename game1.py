581e858491e1234accba7cfa18763681f5f0b6c8
Josh Edmondson <joshedmondson@protonmail.com>
2023-08-22 15:48:27 -0600

    V0.0.2
    Added a basic snail animation along with converting image loads with the .convert()/.convert_alpha() methods.

--- game1.py
+++ game1.py
@@ -9,7 +9,7 @@ pygame.init()
 # Creates a display surface
 screen = pygame.display.set_mode((800,400))
 # Changed the program window title. Displayed ontop of the display surface. 
-pygame.display.set_caption("Game1 : V0.0.1")
+pygame.display.set_caption("Game1 : V0.0.2")
 
 # Sets the variable clock to time.Clock. Which is used to control the game clock for 
 # how fast to runt he game
@@ -23,10 +23,15 @@ test_font  = pygame.font.Font("font/Pixeltype.ttf", 50)
 # test_surface.fill("Red")
 
 # This loads the img sky to the variable sky_img.
-sky_img = pygame.image.load("graphics/Sky.png")
+sky_img = pygame.image.load("graphics/Sky.png").convert()
 
 # This loads the ground img and assigns it to the variable ground_img.
-ground_img = pygame.image.load("graphics/ground.png")
+ground_img = pygame.image.load("graphics/ground.png").convert()
+
+# This loads the snail img and assigns it to the variable snail_img.
+snail_img =pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+# Snails x position.
+snail_x_pos = 800
 
 # Creates a text text variable that uses the render method on the test_font variable.
 # Dont not use anti alias and uses the black color.
@@ -58,6 +63,12 @@ while True:
 
     # Places the text stored in the variable text_surface on the display surface. 
     screen.blit(text_surface, (300,50))
+
+    # Places the image snail_img ontop of the display surface and moves it x 1 pixel each frame. 
+    snail_x_pos += -4
+    screen.blit(snail_img,(snail_x_pos,265))
+    if snail_x_pos < -10:
+       snail_x_pos = 800
     
     # Draws all elements
     # Updated everything
